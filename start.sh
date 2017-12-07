d52f023b056ddc9683c79219e4a21c8d2e6fad32
axemann <axemann@gmail.com>
2017-12-06 20:21:28 -0600

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,1 +1,5 @@
-./xmrig -o $POOL -u $USERNAME.$HOSTNAME/$NANOUSER -p $PASSWORD --max-cpu-usage=$MAXCPU --donate-level=$DONATE
+#!/bin/sh
+set -e
+
+echo "./xmrig -o $POOL -u $WALLET.$HOSTNAME/$NANOUSER -p $PASSWORD --max-cpu-usage=$MAXCPU --donate-level=$DONATE"
+./xmrig -o $POOL -u $WALLET.$HOSTNAME/$NANOUSER -p $PASSWORD --max-cpu-usage=$MAXCPU --donate-level=$DONATE
